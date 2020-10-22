28aa786fece2a5f1c31eb41332a57657560d52bf
BrunoKoppel <bruno.koppel@gmail.com>
2020-10-21 21:27:39 -0400

    Initial Commit

---
+++ CLCV/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CLCV
+//
+//  Created by Bruno Köppel on 10/21/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
